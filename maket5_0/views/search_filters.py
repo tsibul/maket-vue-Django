b407881fc76641280a2c2e82a03d8b4501e2e1b8
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-04-21 02:14:02 +0300

    bug fix search changed o Q

--- maket5_0/views/search_filters.py
+++ maket5_0/views/search_filters.py
@@ -1,21 +1,24 @@
+from django.db.models import Q
+
+
 def order_search_filter(orders, search_string):
     return orders.filter(
-        name__icontains=search_string,
-        address__icontains=search_string,
-        customer_name__icontains=search_string,
-        customer_address__icontains=search_string,
-        customer_inn__icontains=search_string,
+        Q(order_number__icontains=search_string) |
+        # address__icontains=search_string,
+        Q(customer_name__icontains=search_string) |
+        Q(customer_address__icontains=search_string) |
+        Q(customer_inn__icontains=search_string) |
 
-        manager__name__icontains=search_string,
-        manager__mail__icontains=search_string,
-        manager__phone__icontains=search_string,
+        Q(manager__name__icontains=search_string) |
+        Q(manager__mail__icontains=search_string) |
+        Q(manager__phone__icontains=search_string) |
 
-        customer__name__icontains=search_string,
-        customer__address__icontains=search_string,
-        customer__mail__icontains=search_string,
-        customer__phone__icontains=search_string,
-        customer__inn__icontains=search_string,
+        Q(customer__name__icontains=search_string) |
+        Q(customer__address__icontains=search_string) |
+        Q(customer__mail__icontains=search_string) |
+        Q(customer__phone__icontains=search_string) |
+        Q(customer__inn__icontains=search_string) |
 
-        item__name__icontains=search_string,
-        item__print_name__icontains=search_string,
+        Q(orderitem__name__icontains=search_string) |
+        Q(orderitem__print_name__icontains=search_string)
     ).distinct()
