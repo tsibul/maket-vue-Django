efaace11030063fa78f5d829feef4d611a14c457
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-04-25 08:26:32 +0300

    additional file show

--- maket5_0/views/additional_files.py
+++ maket5_0/views/additional_files.py
@@ -1,5 +1,5 @@
 from django.db.models import Q
-from django.http import JsonResponse
+from django.http import JsonResponse, FileResponse, HttpResponse
 from rest_framework.decorators import authentication_classes, permission_classes, api_view
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
@@ -13,7 +13,8 @@ def additional_files_list(request, order_pk):
     order = Order.objects.get(id=order_pk)
     order_number = order.order_number
     main_files = AdditionalFile.objects.filter(
-        order=order
+        order=order,
+        deleted=False,
     ).values(
         'id',
         'name',
@@ -22,7 +23,8 @@ def additional_files_list(request, order_pk):
     )
     deleted_files = AdditionalFile.objects.filter(
         Q(order__order_number=order_number) &
-        ~Q(order=order)
+        ~Q(order=order) &
+        Q(deleted=False)
     ).values(
         'id',
         'name',
@@ -54,3 +56,19 @@ def import_additional_file(request, order_pk):
         'file_type': new_file.file_type
     }
     return JsonResponse(result, safe=False)
+
+
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated])
+def additional_file_show(request, file_pk, file_name):
+    file = AdditionalFile.objects.get(id=file_pk)
+    if file.file_type == '.pdf':
+        try:
+            return FileResponse(open(file.additional_file.path, 'rb'), content_type='application/pdf')
+        except:
+            return None
+    else:
+        try:
+            return FileResponse(open(file.additional_file.path, 'rb'), content_type='application/force-download')
+        except:
+            return None
