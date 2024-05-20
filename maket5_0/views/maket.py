44840b4274cc22d1c1c974b34ec059b3760f278e
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-05-21 02:39:07 +0300

    maket save ready

--- maket5_0/views/maket.py
+++ maket5_0/views/maket.py
@@ -1,9 +1,12 @@
+import datetime
+
 from django.http import JsonResponse
 from rest_framework.decorators import authentication_classes, permission_classes, api_view
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from maket5_0.models import Maket, Order, OrderItem, Good, Color
+from maket5_0.models import Maket, Order, OrderItem, Good, Color, MaketGroup, OrderPrint, PrintPosition, MaketPrint, \
+    PrintColor
 from maket5_0.service_functions import maket_header_info, maket_footer_info, maket_order_items, \
     sort_by_article, maket_show_groups_data, maket_group_patterns_images
 
@@ -116,5 +119,57 @@ def item_color_code_list(request, article):
 @authentication_classes([JWTAuthentication])
 @permission_classes([IsAuthenticated])
 def maket_save(request):
-    return JsonResponse({'id': 'success'}, safe=False)
+    maket_id = request.data['maket_id']
+    order_id = request.data['order_id']
+    order = Order.objects.get(id=order_id)
+    if int(maket_id):
+        maket = Maket.objects.get(id=int(maket_id))
+        maket.date_modified = datetime.date.today()
+    else:
+        maket = Maket(order=order, date_create=datetime.date.today())
+    maket.maket_number = request.data['tech_info']['maketNumber']
+    if not maket.maket_number:
+        maket.maket_number = 1
+    maket.format_selected = request.data['tech_info']['formatSelected']
+    maket.frame_show = request.data['tech_info']['frameShow']
+    maket.comment = request.data['tech_info']['maketComment']
+    maket.picture_show = request.data['tech_info']['pictureShow']
+    maket.table_show = request.data['tech_info']['tableShow']
+    maket.before_footer = request.data['before_footer']
+    maket.save()
 
+    group_data = request.data['group_layout']
+    show_groups = request.data['show_groups']
+    for group in show_groups.keys():
+        maket_group = MaketGroup.objects.filter(name=group, maket=maket).first()
+        if not maket_group:
+            maket_group = MaketGroup(name=group, maket=maket)
+        maket_group.show = show_groups[group]
+        maket_group.show_miniature = group_data[group]['showMiniature']
+        maket_group.spaces_before = group_data[group]['spacesBefore']
+        maket_group.select_all = group_data[group]['selectAll']
+        maket_group.save()
+        order.maket_status = 'P'
+        order.save()
+    item_data = request.data['item_data']
+    for group in item_data.values():
+        for item in group:
+            for prt_item in item['print_item']:
+                print_item = OrderPrint.objects.get(id=prt_item['id'])
+                print_position = PrintPosition.objects.get(id=prt_item['position_id'])
+                print_item.print_position = print_position
+                print_item.save()
+                maket_print = MaketPrint.objects.filter(print_item=print_item, maket=maket).first()
+                if not maket_print:
+                    maket_print = MaketPrint(print_item=print_item, maket=maket)
+                try:
+                    print_checked = prt_item['checked']
+                    maket_print.checked = print_checked
+                except:
+                    maket_print.checked = False
+                maket_print.save()
+                for color in prt_item['color']:
+                    print_color = PrintColor.objects.get(id=color['id'])
+                    print_color.pantone = color['pantone']
+                    print_color.save()
+    return JsonResponse({'id': maket.id}, safe=False)
