f18d8c9040bce14a7983dd07d279500fd551f49a
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-06-02 00:09:56 +0300

    group to film with reformat date

--- maket5_0/views/maket_list.py
+++ maket5_0/views/maket_list.py
@@ -1,13 +1,13 @@
 import os
 
 from django.core.files import File
-from django.db.models import Count, Q
+from django.db.models import Count, Q, CharField, Value, F, Func
 from django.http import JsonResponse, FileResponse
 from rest_framework.decorators import authentication_classes, permission_classes, api_view
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from maket5_0.models import Maket, Order, MaketGroup
+from maket5_0.models import Maket, Order, MaketGroup, Film
 from maket5_0.views import order_search_filter
 
 
@@ -40,6 +40,38 @@ def maket_list_info(request, search_string, sh_deleted, id_no):
     result = []
     order_prev = 0
     for order in order_list:
+        maket_groups = list(MaketGroup.objects.filter(
+            maket__id=order['maket__id'],
+            show=True
+        ).order_by(
+            'name'
+        ).values(
+            'id',
+            'name',
+        ))
+        for group in maket_groups:
+            film_list = Film.objects.filter(groupinfilm__group__id=group['id'], deleted=False).order_by('-date')
+            film_list_out = list(film_list.annotate(
+                dateCreate=Func(
+                    F('date'),
+                    Value('DD.MM.YY'),
+                    function='to_char',
+                    output_field=CharField()
+                ),
+                dateSent=Func(
+                    F('date_sent'),
+                    Value('DD.MM.YY'),
+                    function='to_char',
+                    output_field=CharField()
+                ),
+            ).values(
+                'film_number',
+                'dateCreate',
+                'dateSent',
+                'status',
+                'groupinfilm__id',
+            ))
+            group['films'] = film_list_out
         if order['id'] != order_prev:
             order_prev = order['id']
             new_order = {
@@ -57,12 +89,7 @@ def maket_list_info(request, search_string, sh_deleted, id_no):
                         'comment': order['maket__comment'],
                         'file': order['maket__file'],
                         'maketDeleted': order['maket__deleted'],
-                        'groups': list(MaketGroup.objects.filter(
-                            maket__id=order['maket__id'],
-                            show=True
-                        ).order_by(
-                            'name'
-                        ).values_list('name', flat=True))
+                        'groups': maket_groups
                     }
                 ]
             }
@@ -76,12 +103,7 @@ def maket_list_info(request, search_string, sh_deleted, id_no):
                     'comment': order['maket__comment'],
                     'file': order['maket__file'],
                     'maketDeleted': order['maket__deleted'],
-                    'groups': list(MaketGroup.objects.filter(
-                        maket__id=order['maket__id'],
-                        show=True
-                    ).order_by(
-                        'name'
-                    ).values_list('name', flat=True))
+                    'groups': maket_groups
                 }
             )
         new_order['maketQuantity'] = len(list(filter(lambda x: x['file'], new_order['maketList'])))
@@ -98,7 +120,7 @@ def maket_file_save(request, maket_id):
     :return:maket_file_save/<int:maket_id>
     """
     maket = Maket.objects.get(id=maket_id)
-    file_name = ('Макет_' + maket.order.customer.name.replace('"','').replace(' ', '_')
+    file_name = ('Макет_' + maket.order.customer.name.replace('"', '').replace(' ', '_')
                  + '_' + maket.order.order_number + '_' + str(maket.maket_number) + '.pdf')
 
     with open('maket5_0/files/tmp_file', 'rb') as f:
