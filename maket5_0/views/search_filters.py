45bb5e9a4df362ab60403db08b4734aa298df953
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-06-05 11:38:53 +0300

    if search - number < 100 search only film number if search dd.mm.yy looking for dates

--- maket5_0/views/search_filters.py
+++ maket5_0/views/search_filters.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db.models import Q
 
 
@@ -24,5 +26,28 @@ def order_search_filter(orders, search_string):
     ).distinct()
 
 
-def film_search_filter(film_list, search_string):
-    return film_list
+def film_search_filter(film_list, search_string, sh_deleted):
+    try:
+        date_object = datetime.datetime.strptime(search_string, '%d.%m.%y').date()
+    except ValueError:
+        date_object = None
+    try:
+        film_no = int(search_string)
+    except ValueError:
+        film_no = None
+
+    if film_no and film_no < 100:
+        return film_list.filter(film_number__icontains=film_no)
+    else:
+        return film_list.filter(
+            (Q(date=date_object) if date_object else Q()) |
+            (Q(date_sent=date_object) if date_object else Q()) |
+
+            (Q(groupinfilm__group__maket__order__order_number__icontains=search_string) |
+             Q(groupinfilm__group__maket__order__customer__name__icontains=search_string) |
+             Q(groupinfilm__group__name__icontains=search_string) |
+             (Q(groupinfilm__group__maket__order__order_date=date_object) if date_object else Q())
+             ) &
+            (Q(groupinfilm__status=True) if not sh_deleted else Q())
+
+        ).distinct()
